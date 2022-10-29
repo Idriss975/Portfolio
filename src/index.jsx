57e709eb5138ac259d6c6e35af34f525247ba6c8
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2022-10-29 13:16:23 +0100

    .

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 if (location.hostname.split('.')[0] === "cv")
     location.href = "https://www.berchil.in/assets/documents/Berchil_Idriss_CV.pdf";
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+else
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
