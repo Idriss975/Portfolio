26c674a2d30aa78aba01115a2d3cee2d1e66f83a
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2022-10-29 13:15:10 +0100

    .

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import './lang/i18n';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+if (location.hostname.split('.')[0] === "cv")
+    location.href = "https://www.berchil.in/assets/documents/Berchil_Idriss_CV.pdf";
 ReactDOM.render(
   <React.StrictMode>
     <App />
